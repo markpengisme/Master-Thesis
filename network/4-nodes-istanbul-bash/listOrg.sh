44dfedeeeb5863a69d0d52d56ade98c348e9da2c
Mark Peng <markpengisme@gmail.com>
2021-03-15 11:45:18 +0800

    upload tool listOrg.sh

--- network/4-nodes-istanbul-bash/listOrg.sh
+++ network/4-nodes-istanbul-bash/listOrg.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-num=$(ls network/*/qdata/dd*/keystore/key | wc -l | awk '$1=$1')
+num=$(ls qdata/dd*/keystore/key | wc -l | awk '$1=$1')
 
 for n in $(seq 1 $num)
 do
-    ip=$(cat network/*/config.json | \
+    ip=$(cat config.json | \
         grep ip | awk '$1=$1' | sed -n ${n}p)
-    wsport=$(cat network/*/config.json | \
+    wsport=$(cat config.json | \
         grep wsPort | awk '$1=$1' | sed -n ${n}p)
-    key=$(cat network/*/qdata/dd${n}/keystore/key | \
+    key=$(cat qdata/dd${n}/keystore/key | \
         awk '{split($0, a, "\"")}{print a[2]":0x"a[4]}')
     echo $ip $wsport $key
 done
